a600af4d1a9dde6bfce38e06f16adad650619c2a
tomohirokkk <kt3dprinter@icloud.com>
2015-04-04 19:38:47 +0900

    本番用

---
+++ 1_servo/servo_test2.py
@@ -0,0 +1,63 @@
+import webiopi
+import time
+
+webiopi.setDebug()
+
+GPIO = webiopi.GPIO
+SERVO_X = 23
+DEFAULT_X = 0
+CURRENT_X = DEFAULT_X
+time_stamp = time.time()
+
+def setup():
+    GPIO.setFunction(SERVO_X, GPIO.PWM)
+    GPIO.pwmWriteAngle(SERVO_X, DEFAULT_X)
+    webiopi.sleep(0.5)
+    GPIO.pulseRatio(SERVO_X, 0)
+
+def loop():
+   global time_stamp
+   if (time_stamp <= time.time() - 20):
+       defaultPosition()
+       webiopi.sleep(0.5)
+
+@webiopi.macro
+def defaultPosition():
+    global time_stamp
+    if time_stamp <= time.time() - 0.2 and (CURRENT_X != DEFAULT_X):
+        time_stamp = time.time()
+        global CURRENT_X
+        CURRENT_X = DEFAULT_X
+        GPIO.pulseRatio(SERVO_X, 1)
+        GPIO.pwmWriteAngle(SERVO_X, 0)
+        webiopi.sleep(0.5)
+        GPIO.pulseRatio(SERVO_X, 0)
+        webiopi.sleep(0.5)
+        time_stamp = time.time()
+
+@webiopi.macro
+def left():
+    global CURRENT_X, time_stamp
+    if CURRENT_X < DEFAULT_X + 5*10 and time_stamp <= time.time() - 0.2:
+        GPIO.pulseRatio(SERVO_X, 1)
+        time_stamp = time.time()
+        CURRENT_X = CURRENT_X + 15
+        GPIO.pwmWriteAngle(SERVO_X, CURRENT_X)
+        webiopi.sleep(0.5)
+        time_stamp = time.time()
+        GPIO.pulseRatio(SERVO_X, 0)
+
+@webiopi.macro
+def right():
+    global CURRENT_X, time_stamp
+    if CURRENT_X > DEFAULT_X - 5*10 and time_stamp <= time.time() - 0.2:
+        GPIO.pulseRatio(SERVO_X, 1)
+        time_stamp = time.time()
+        CURRENT_X = CURRENT_X - 15
+        GPIO.pwmWriteAngle(SERVO_X, CURRENT_X)
+        webiopi.sleep(0.5)
+        time_stamp = time.time()
+        GPIO.pulseRatio(SERVO_X, 0)
+
+def destroy():
+    GPIO.setup(SERVO_X, GPIO.IN)
